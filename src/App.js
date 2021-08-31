393b9e81a454d1b2fc3090e33e9d8054e2f1089e
anggitrestu <anggitrestu60@gmail.com>
2021-08-31 22:43:50 +0700

    initial init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginPage from './LandingPage/LoginPage';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage></LoginPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LandingPage/LoginPage.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import useStyles from '../Parts/useStyles';
+import Copyright from '../Parts/Copyright';
+import Container from '@material-ui/core/Container';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { width } from '@material-ui/system';
+export default function LoginPage() {
+  const classes = useStyles();
+  const [state, setState] = useState({
+    age: '',
+    name: 'hai',
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl className={classes.formControl} fullWidth>
+                <NativeSelect
+                  xs={12}
+                  value={state.age}
+                  onChange={handleChange}
+                  name="age"
+                  className={classes.selectEmpty}
+                  inputProps={{ 'aria-label': 'age' }}
+                >
+                  <option value=""> --Login Sebagai--</option>
+                  <option value={10}>Tutor</option>
+                  <option value={20}>Learner</option>
+                </NativeSelect>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
