6aaf9ba4dadfea6d942848e652ebc0a30d4d2e93
furqondinata <fdonata123@gmail.com>
2021-09-01 21:47:50 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path="/register">
           <Register />
         </Route>
-        <Route path="/login">
+        <Route>
           <LoginPage />
         </Route>
       </Switch>
