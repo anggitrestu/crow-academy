d94e08b04ba72e89c56c5875b46f03be3324a7b1
anggitrestu <anggitrestu60@gmail.com>
2021-09-11 02:41:36 +0700

    add tailwind css

--- src/Parts/Home.js
+++ src/Parts/Home.js
@@ -3,27 +3,25 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <ul>
-          <li>
-            <Link to={`/login`} className="btn btn-primary">
-              <h3>Halaman Login</h3>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/register`} className="btn btn-primary">
-              <h3>Halaman Register</h3>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/tutor`} className="btn btn-primary">
-              <h3>Halaman Tutor</h3>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
+    <div style={{ textAlign: 'center' }}>
+      <ul>
+        <li>
+          <Link to={`/login`} className="btn btn-primary">
+            <h3>Halaman Login</h3>
+          </Link>
+        </li>
+        <li>
+          <Link to={`/register`} className="btn btn-primary">
+            <h3>Halaman Register</h3>
+          </Link>
+        </li>
+        <li>
+          <Link to={`/tutor`} className="btn btn-primary">
+            <h3>Halaman Tutor</h3>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
